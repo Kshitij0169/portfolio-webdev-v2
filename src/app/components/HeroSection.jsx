6707d21989d2637d41aa7aded42c093b3d342390
Kshitij0169 <tiwari.ks@northeastern.edu>
2023-10-02 01:03:34 -0400

    UI updates

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
                 animate={{opacity: 1, scale: 1 }} 
                 transition={{duration: 0.5}}
                 className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl  font-extrabold" >
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold" >
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-700 to-secondary-500">
                     Hey, I am{" "}
                     </span>
@@ -28,11 +28,11 @@ const HeroSection = () => {
                     sequence={[
                         "Kshitij",
                         1000,
-                        "a Developer",
+                        "Developer",
                         1100,
-                        "a UI/UX Designer",
+                        "UI/UX Designer",
                         1100,
-                        "a Gymrat",
+                        "Gymrat",
                         1100
                     ]}
                     wrapper="span"
