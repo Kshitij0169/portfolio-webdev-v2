be11f5bbb19460c7061e819b091fe315d067321c
Kshitij0169 <tiwari.ks@northeastern.edu>
2023-10-02 11:14:26 -0400

    mobile ui changes

--- src/app/components/Achievements.jsx
+++ src/app/components/Achievements.jsx
@@ -32,7 +32,7 @@ const achievementsList = [
 
 const Achievements = () => {
   return (
-    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+    <div className="py-8 px-4 sm:py-16 xl:px-16">
         <div className="border=[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
         {
             achievementsList.map((achievement, index) => {
